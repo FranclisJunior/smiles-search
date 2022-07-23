5d2c7a0c8861ddd61e57c9ffd2c835ce416d4f93
Franclis Galdino <franclis.silva@virtus.ufcg.edu.br>
2022-07-23 00:14:30 -0300

    Make end date optional

--- src/app/components/search-flights /search-flights.component.ts
+++ src/app/components/search-flights /search-flights.component.ts
@@ -5,7 +5,7 @@ import { Validators } from '@angular/forms';
 import { CrudService } from '../../core/service/crud.service';
 import { NotificationService } from '../../core/service/notification.service';
 import { AppInjector } from '../../app.injector';
-
+import * as moment from 'moment';
 @Component({
     selector: 'app-search-flights',
     templateUrl: './search-flights.component.html',
@@ -38,7 +38,7 @@ export class SearchFlightsComponent implements OnInit {
         origin: this.formBuilder.control(undefined, [Validators.required]),
         destination: this.formBuilder.control(undefined, [Validators.required]),
         startDate: this.formBuilder.control(undefined, [Validators.required]),
-        endDate: this.formBuilder.control(undefined, [Validators.required]),
+        endDate: this.formBuilder.control(undefined, []),
       };
     }
 
@@ -46,8 +46,11 @@ export class SearchFlightsComponent implements OnInit {
       const origin = this.editForm.value.origin.toUpperCase();
       const destination = this.editForm.value.destination.toUpperCase();
       const startDate = this.editForm.value.startDate;
-      const endDate = this.editForm.value.endDate;
+      const endDate = this.editForm.value.endDate ? this.editForm.value.endDate : moment(startDate).add(1, "M").format('YYYY-MM-DD');
+      console.log(endDate)
       const URL = `/calendar/month?originAirportCode=${origin}&destinationAirportCode=${destination}&startDate=${startDate}&endDate=${endDate}&startDate2=${startDate}&endDate2=${endDate}&departureDate=${startDate}&returnDate=${endDate}&adults=1&children=0&infants=0&forceCongener=false&cabin=ALL&bestFare=true`
+         
+      
       this.loading = true
       this.service.get(URL)
       .subscribe(
